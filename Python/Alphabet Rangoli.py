e66671520fc348c952dba3049faea485a293c76e
Suraj Zinzuwadia <suraj.zinzuwadia@gmail.com>
2019-09-11 23:20:27 +0530

    Adding Alphabet rangoli problem solution

---
+++ Python/Alphabet Rangoli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import string
+N = int(input())
+mid = N - 1
+for i in range(N - 1, 0, -1):
+    row = ['-'] * (2 * N - 1)
+    for j in range(N - i):
+        row[mid - j] = row[mid + j] = string.ascii_lowercase[j + i]
+    print( '-'.join(row))
+for i in range(0, N):
+    row = ['-'] * (2 * N - 1)
+    for j in range(0, N - i):
+        row[mid - j] = row[mid + j] = string.ascii_lowercase[j + i]
+    print ('-'.join(row))
+
+
+# In[ ]:
+
+
+
+
