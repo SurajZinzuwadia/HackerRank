51d3d1cd19b84d1a1ce8bcf9296fca801531e637
Suraj Zinzuwadia <suraj.zinzuwadia@gmail.com>
2020-04-30 22:43:44 +0530

    modified quicksort

--- Problem solving/Algorithms/Quicksort 1 - Partition/Quicksort 1 - Partition.py
+++ Problem solving/Algorithms/Quicksort 1 - Partition/Quicksort 1 - Partition.py
@@ -3,24 +3,19 @@ def quickSort(arr):
     left = []
     right = []
     equal = [p]
-    result = []
-    for i in range(1,n):
-        p = arr[0]
-        left = []
-        right = []
-        equal = [p]
-        for i in range(1, len(arr)):
-            if (p < arr[i]):
-                right.append(arr[i])
-            elif (p > arr[i]):
-                left.append(arr[i])
-            else:
-                equal.append(arr[i])
-        # if(len(left) > 1):
-        #     left = quickSort(left)
-        # if(len(right) > 1):
-        #     right = quickSort(right)
-        return (left + equal + right)
+    for i in range(1, n):
+        if (p < arr[i]):
+            right.append(arr[i])
+        elif (p > arr[i]):
+            left.append(arr[i])
+        else:
+            equal.append(arr[i])
+    # if(len(left) > 1):
+    #     left = quickSort(left)
+    # if(len(right) > 1):
+    #     right = quickSort(right)
+    return (left + equal + right)
+
 if __name__ == '__main__':
 
     n = int(input())
