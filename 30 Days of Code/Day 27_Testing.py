497f52284f635efac4d2310a64e81f1b6b3657f7
Suraj Zinzuwadia <suraj.zinzuwadia@gmail.com>
2020-05-14 17:02:15 +0530

    27,28 done

---
+++ 30 Days of Code/Day 27_Testing.py
@@ -0,0 +1,29 @@
+class TestDataEmptyArray(object):
+
+    @staticmethod
+    def get_array():
+        # complete this function
+        return list()
+
+
+class TestDataUniqueValues(object):
+
+    @staticmethod
+    def get_array():
+        return [5, 2, 8, 3, 1, -6, 9]
+
+    @staticmethod
+    def get_expected_result():
+        return 5
+
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    @staticmethod
+    def get_array():
+        return [5, 2, 8, 3, 1, -6, 9, -6, 10]
+
+    @staticmethod
+    def get_expected_result():
+        return 5
+
---
+++ 30 Days of Code/Day 28_RegEx, Patterns, and Intro to Databases.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+
+
+    lst=[]
+    for a0 in range(int(input())):
+        firstName, emailID = [str(s) for s in input().split()]
+        if re.search('@gmail\.com$', emailID):
+            lst.append(firstName)
+    print(*sorted(lst), sep='\n')
