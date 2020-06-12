5bddc5c130832143be51368a16f035670a15480c
Suraj Zinzuwadia <suraj.zinzuwadia@gmail.com>
2020-06-12 21:09:20 +0530

    Mark and Toys

---
+++ Problem solving/Algorithms/Mark and Toys/Mark and Toys.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    count = 0
+    for i in range(0,len(prices)):
+        k = k - prices[i]
+        if (k < 0):
+            break
+        count+=1
+    print(count)
+
+if __name__ == '__main__':
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+
